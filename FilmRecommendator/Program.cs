9147cdc90bebe0d725982e1badb83553bb50a949
valglez <vsalglez@gmail.com>
2023-10-11 08:57:27 +0200

    Added MongoDB dependencies

--- FilmRecommendator/Program.cs
+++ FilmRecommendator/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Application
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
