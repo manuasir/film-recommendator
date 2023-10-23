6ac26973671a90c941271e71a4093c94b043c36f
Santiagoestremadoyro <estremadoyros@gmail.com>
2023-10-23 15:46:51 +0200

    apiupdate

--- src/database/api.py
+++ src/database/api.py
@@ -1,47 +1,41 @@
+from database import connection
+from database.models import film
 import requests
-from dotenv import load_dotenv
-from index import conn
-from models.film import Film
 import os
 
-load_dotenv()
+class API:
+    def __init__(self, api_key, base_url, start_id, end_id):
+        self.api_key = api_key
+        self.base_url = base_url
+        self.params = {"api_key": self.api_key}
+        self.start_id = start_id
+        self.end_id = end_id
 
-#Clave de la API de TMDb (token)
-api_key = "2ffcfdf1b9901801d0659954f796424c"
-
-#URL base de la API de TMDb
-base_url = "https://api.themoviedb.org/3"
-
-#Parámetros de la solicitud a la API
-params = {
-    "api_key": api_key,
-}
-
-#ID inicial de película o serie
-start_id = 1  
-
-#ID final de película o serie
-end_id = 1000  # Puedes aumentar si se requieren mas cantidad de peliculas
-
-for movie_id in range(start_id, end_id + 1):
-    #solicitud a la API de TMDb para obtener información de una película o serie
-    response = requests.get(f"{base_url}/movie/{movie_id}", params=params)
-
-    #Comprobar si la solicitud fue exitosa(En el protocolo HTTP, el código de estado "200 OK" indica que la solicitud fue exitosa)
-    if response.status_code == 200:
-        #Procesar los datos de la película o serie
-        movie_data = response.json()
-
-        #Crear un objeto Film con los datos necesarios
-        film = Film(
-            title=movie_data["title"],
-            year=movie_data["release_date"].split("-")[0],
-            director=movie_data["director"],  #obtener el director desde la API
-            genre=movie_data["genres"],  #obtener los géneros desde la API
-            summary=movie_data.get("overview", "")  #Resumen de la película
-        )
-
-        # Insertar el objeto Film en la base de datos MongoDB
-        conn.coll.insert_one(film.__dict__)
-
-print("Datos insertados en la base de datos.")
+    def insert_films(self):
+        try:
+            conn = connection.Database(
+                os.getenv("DB_URI"), os.getenv("DB_NAME"), os.getenv("DB_COLL")
+            )
+            for movie_id in range(self.start_id, self.end_id + 1):
+                # solicitud a la API de TMDb para obtener información de una película o serie
+                response = requests.get(f"{self.base_url}/movie/{movie_id}", params=self.params)
+                # PENDIENTE
+                # solicitud de créditos para extraer al director
+                # credits = requests.get(f"{self.base_url}/movie/{movie_id}/credits", params=self.params)
+                # Comprobar si la solicitud fue exitosa (En el protocolo HTTP, el código de estado "200 OK" indica que la solicitud fue exitosa)
+                if response.status_code == 200:
+                    # Procesar los datos de la película o serie
+                    movie_data = response.json()
+                    # Crear un objeto Film con los datos necesarios
+                    sample = film.Film(
+                        title=movie_data["title"],
+                        year=movie_data["release_date"].split("-")[0],
+                        director=movie_data["director"], # PENDIENTE
+                        genre=movie_data["genres"],  # obtener los géneros desde la API
+                        summary=movie_data.get("overview", "")  # Resumen de la película
+                    )
+                    # Insertar el objeto Film en la base de datos MongoDB
+                    conn.coll.insert_one(sample.__dict__)
+                    print("Datos insertados en la base de datos.")
+        except Exception as e:
+            print(f"Error: {e}")
