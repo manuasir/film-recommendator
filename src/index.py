6f84bf8b9e251351d404809557c97721e4290234
valglez <vsalglez@gmail.com>
2023-10-29 01:18:45 +0200

    Changed import module for db connection

--- src/index.py
+++ src/index.py
@@ -1,4 +1,4 @@
-from database import mongo
+from database import connection
 from api import api
 from dotenv import load_dotenv
 import os
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 # Conexión con MongoDB
-conn = mongo.Database(os.getenv("DB_URI"), os.getenv("DB_NAME"), os.getenv("DB_COLL"))
+conn = connection.Database(os.getenv("DB_URI"), os.getenv("DB_NAME"), os.getenv("DB_COLL"))
 
 # Llamada a la API
 api_instance = api.API(conn, os.getenv("API_KEY"), "https://api.themoviedb.org/3", 1, 5)
